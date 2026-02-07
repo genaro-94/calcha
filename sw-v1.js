b024dd6292b4baf1bd8092ac676a5a65d6e77309
genaro-94 <genaritono14@gmail.com>
2026-02-07 00:23:49 -0300

    Update sw-v1.js

--- sw-v1.js
+++ sw-v1.js
@@ -1,25 +1,25 @@
-const VERSION = "v1";
-const CACHE_NAME = `calcha-cache-${VERSION}`;
 
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./Icon-192.png",
-  "./Icon-512.png"
+
+const CACHE_NAME = "calcha-pwa-v3";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/comercios.json",
+  "/Icon-192.png",
+  "/Icon-512.png"
 ];
 
-// INSTALL
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// ACTIVATE
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -31,17 +31,7 @@ self.addEventListener("activate", e => {
   self.clients.claim();
 });
 
-// FETCH
 self.addEventListener("fetch", e => {
-  const url = e.request.url;
-
-  // JSON siempre de red
-  if (url.endsWith(".json")) {
-    e.respondWith(fetch(e.request));
-    return;
-  }
-
-  // App shell: cache first
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
